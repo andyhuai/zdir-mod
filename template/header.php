3ed019bb83af1c6a2f2b52019be992f9f8993e83
andyhuai <huangxiaohuai@nstc.com.cn>
2023-01-18 11:50:36 +0800

    remove statics and ad

--- template/header.php
+++ template/header.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8" />
 	<title><?php echo $siteinfo['title']; ?></title>
 	<meta name="generator" content="EverEdit" />
-	<meta name="author" content="xiaoz.me" />
 	<meta name="keywords" content="<?php echo $siteinfo['keywords']; ?>" />
 	<meta name="description" content="<?php echo $siteinfo['description']; ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,23 +11,8 @@
 	<link rel="stylesheet" href="./static/layui/css/layui.css">
 	<link rel='stylesheet' href='./static/style.css?v=<?php echo $version; ?>'>
 	<link rel="stylesheet" href="./static/font-awesome/css/font-awesome.min.css">
-	<?php
-		//判断统计代码是否存在
-		if( file_exists('./static/tongji.js') ) {
-	?>
-	<script type="text/javascript" src="./static/tongji.js"></script>
-	<?php } ?>
 </head>
 <body>
-	<!--全站两侧广告位-->
-	<?php if( file_exists('./config.json') ){
-		$gg = @file_get_contents('./config.json');
-		$gg = json_decode($gg);
-	?>
-	<div id="gg-left"><?php echo $gg->gg->gg_left; ?></div>
-    <div id="gg-right"><?php echo $gg->gg->gg_right; ?></div>
-    <?php } ?>
-    <!--全站两侧广告位END-->
 	<!--顶部导航栏-->
 	<div class = "header">
         <div class = "layui-container">
